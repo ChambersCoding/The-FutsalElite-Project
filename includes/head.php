4e56a6ff90eb22ec28c5b743c03530c4253c35de
RamonChambers <RamonChambers@users.noreply.github.com>
2015-11-25 22:19:31 +0000

    Create head.php

---
+++ includes/head.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+    <head>
+        <title>FutsalElite</title>
+		<link rel="shortcut icon" href="images/icons/favicon.ico">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <meta name="keywords" content="Football, Futsal, Brazilian Football, Elite, Futsal Elite"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+         <!-- The CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/header_style.css" type="text/css" media="screen"/>
+       	<link rel="stylesheet" href="css/main_style.css">
+		<link rel="stylesheet" href="css/sss.css" type="text/css" media="all">
+		<link rel="stylesheet" type="text/css" href="css/testimonial_style.css" />
+       	
+
+         <!-- The JavaScript -->
+         
+	    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+		<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+		<script type="text/javascript" src="js/sss.js"></script>
+		<script type="text/javascript" src="js/1.5/jquery.min.js"></script>
+		<script type="text/javascript" src="js/jquery.cycle.all.latest.js"></script>
+
+		<script>
+		jQuery(function($) {
+		$('.slider').sss({
+		slideShow : true, // Set to false to prevent SSS from automatically animating.
+		startOn : 0, // Slide to display first. Uses array notation (0 = first slide).
+		transition : 400, // Length (in milliseconds) of the fade transition.
+		speed : 6500, // Slideshow speed in milliseconds.
+		showNav : true // Set to false to hide navigation arrows.
+			});
+		$('#testimonials')
+		.after('<div id="nav2">')
+		.cycle({
+        fx: 'scrollRight', // choose your transition type, ex: fade, scrollUp, scrollRight, shuffle
+		pager:  '#nav2',
+		timeout: 10000
+
+     		});
+		});
+
+		</script>
+
+
+
+    </head>
+
+    <body>
