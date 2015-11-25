82188c642c810d2997e015edaa3b747179d79484
RamonChambers <RamonChambers@users.noreply.github.com>
2015-11-25 21:50:21 +0000

    Create training.php

---
+++ training.php
@@ -0,0 +1,61 @@
+
+<?php 
+
+include 'includes/head.php';
+include 'includes/menu.php';
+
+?>	
+
+<div class="maincontainer col-lg-10 col-md-10 col-sm-10 col-xs-10"><!--  maincontainer starts here -->
+
+	<h1>1:1 Training </h1>
+
+	<p>For all ages we offer a personalised quality service, guaranteeing results for the individual whether it be Football/Futsal related or strengthening and conditioning. 
+	We endeavour to develop performance for being suitable to fulfil a particular role or task desired by the individual.</p>
+	<div >
+	<img src="images/training/1.png" style="width: 310px; height: 250px; padding: 0px 20px 20px 0px;">
+	<img src="images/training/2.png" style="width: 310px; height: 250px; padding: 0px 20px 20px 0px;">
+	<img src="images/training/3.png" style="width: 310px; height: 250px; padding: 0px 20px 20px 0px;">
+	</div>
+
+	<h1>Testimonials</h1>
+
+				<div class="training_testimonials">
+				<div style="width:250px; float:left; ">
+				<blockquote>
+				<p>Kenny has been working with my son Kieran, 17, since an early age, providing both coaching and football mentoring support. 
+				I have often braved the elements to stand and observe Kenny's innovative training routines which have developed Kieran's technique,
+				 football brain and lately his physical attributes. 
+				For example, regarding the latter, Kenny has been working with Kieran to improve his core strength, agility, fitness and acceleration, much-needed tools as 
+				he is slight-framed. </p>
+
+				<p>
+				After only a few weeks into this training schedule Kieran showed a marked improvement and suddenly found that he was able to cope physically 
+				against boys he was previously struggling with. I have also observed Kenny in game situations and he is a brilliant coach who reaches out to his
+				players far better than any of the Sunday league football coaches I have seen. 
+				I would say that he is equipped to coach or manage at the highest level in the professional game and cannot recommend him highly enough.
+				<cite>&ndash;Cassius Powell</cite></p></blockquote></div>	
+				
+				<div style="width:250px; float:left;">
+				<blockquote>
+				<p>My sons aged 8 and 10 years have had 1:1 sessions for the past 3 months. 
+				They have worked on several techniques; also improving their concentration and general physical development.
+				1:1 sessions has allowed to work on their individual weaknesses. </p> 
+				<p> In such a short time I have seen a great improvement and confidence growth.
+				I would recommend them to anyone and for me it is no surprise that Futsal Elite have developed so many players.
+				<cite >&ndash;Harina</cite></p></blockquote>
+				</div>
+				
+				<div style="width:250px; float:left;">
+				<blockquote >
+				<p>A perfect blend of technical coaching alongside development of agility and athleticism. </p>
+				<p>The coaching is innovative and imaginative, with a great focus on developing skill and encouraging creativity, but also teaching game awareness.
+				My son has come on leaps and bounds in the last two years thanks to the hard work, dedication and positive environment created by the wonderful coaching.
+				<cite>&ndash;Zeeshan Siddique</cite></p></blockquote>  </div>
+
+				</div>
+
+<?php include 'includes/footer.php'; ?>
+
+	
+</div><!--  maincontainer ends here -->
