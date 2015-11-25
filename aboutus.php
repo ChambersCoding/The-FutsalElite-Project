2ab2cde6100cad5a4616068d8af41e6a66c48d21
RamonChambers <RamonChambers@users.noreply.github.com>
2015-11-25 21:46:19 +0000

    Create aboutus.php

---
+++ aboutus.php
@@ -0,0 +1,93 @@
+
+<?php 
+
+include 'includes/head.php';
+include 'includes/menu.php';
+
+?>	
+
+<div class="maincontainer col-lg-10 col-md-10 col-sm-10 col-xs-10"><!--  maincontainer starts here -->
+
+<h1>About Us </h1>
+	<div style="height:560px;">
+	<div class="col-xs-0 col-sm-0  col-md-0 col-lg-6 ">
+	<img src="images/express_model.png" style="width: 100%; height: 500px; padding-right: 20px;">
+	</div>
+
+
+
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6" >
+	<h3>What is Futsal?</h3>
+	<p>Futsal is intense and exhilarating, as defence turns into attack at breath-taking speed, an action packed game which keeps players physically active whilst having fun is born!</p>
+	<p>Futsal is played with a half size and heavier ball on a small court. Having a small playing area forces all players to fully participate and be involved, which in turn fast-tracks 
+	the attainment of technique.</p>
+
+	<p>Futsal began in the South American countries of Uruguay and Brazil where, in the 1930’s, two versions of Small Sided Soccer were being played in 
+	the cities of Montevideo and Sao Paulo respectively. In Brazil, the large crowded cities and a shortage of playing pitches forced a soccer mad populace to play small sided soccer. 
+	A version began to develop on the streets of Sao Paulo, leading to the publishing of the first rules of the game in 1936 from the country that would soon become the masters of the game. 
+	Skills and techniques honed in Futsal soon began to become apparent in the performance of Brazil’s National Team – Pele, Rivelino and Zico all played the game at some time in their 
+	development.</p> 
+
+	<p>The adoption of the game by FIFA has led to its spread throughout the world. There are large numbers of registered players in Europe, 
+	South America and Asia. There are more than 50 national leagues and even Professional Futsal leagues in Brazil, Iran, Japan, Italy, Portugal, Spain & Russia. 
+	Futsal gets terrestrial TV coverage in Spain, Italy, Brazil, Croatia, Romania, Ukraine and Russia.</p>
+
+	<h3>What Futsal means to us?</h3>
+	<p>The sport places an emphasis on greater concentration, responsiveness, the improvement of skilful dribbling, accurate passing and ball control. 
+	Moreover, Futsal generates an environment that demands players to think quickly and make quick decisions.</p>
+	
+	<p>In all Futsal injects quality skills, game awareness and speed into player’s blood, mind, body and soul, improving Agility, Balance, Co-ordination and Speed.</p>
+	</div>
+	</div>
+	<br><br>
+	<strong><center><p style="font-size:25px;">The best have benefited and you can too, from a sport which celebrates “Creativity and Imagination”.</p></center></strong> 
+	<br>
+
+
+	<div class="professionalquotes">
+	<img src="images/messi.jpg" />
+	<p style="font-style: italic;">“When I was a young boy in Argentina, I played Futsal in the street and in a club. It was great fun and it helped me become the player I am today” </p>
+	<strong>- Lionel Messi</strong>
+	</div>
+	
+	<div class="professionalquotes">
+	<img src="images/ronaldo.jpg" />
+	<p style="font-style: italic;">“In Portugal, all we played growing up was Futsal.
+	The smaller court helped my footwork skills, the nature of the game made me feel so free when I played. If it wasn’t for Futsal, I would definitely not be the player I am today.” </p>
+	<strong>- Christiano Ronaldo </strong>
+	</div>
+
+	<div class="professionalquotes">
+	<img src="images/neymar.jpg" />
+	<p style="font-style: italic;">“Futsal helps a lot because you need to think quickly. 
+	It's a more dynamic game and today in Europe there's not much space so you need to think quicker and futsal has helped me a lot with that. Futsal is one of my passions.” </p>
+	<strong>- Neymar </strong>
+	</div>
+
+	<div class="professionalquotes">
+	<img src="images/ronaldinho.jpg" />
+	<p style="font-style: italic;"> “When you come to play normal soccer, it’s easier if you’ve come from Futsal” </p>
+	<strong>- Ronaldinho </strong>
+	</div>
+
+	<div class="professionalquotes">
+	<img src="images/pele.jpg" />
+	<p style="font-style: italic;">“I played Futsal growing up in Baurd. In Futsal you need to think quick and play quick so it’s easier for you when you move to normal football” </p>
+	<strong>- Pele </strong>
+	</div>
+
+	<div class="professionalquotes">
+	<img src="images/zidane.jpg" />
+	<p style="font-style: italic;">"The technique of Futsal is different. There are spectacular things you can attempt that you wouldn't dare in Football" </p>
+	<strong>- Zidane </strong>
+	</div>
+
+	<div class="professionalquotes">
+	<img src="images/xavi.jpg" />
+	<p style="font-style: italic;">"In Futsal you see whether a player is really talented. You notice the small details in quality, class & tactical understanding" </p>
+	<strong>- Xavi </strong>
+	</div>
+	
+<?php include 'includes/footer.php'; ?>
+	
+</div><!--  maincontainer ends here -->
