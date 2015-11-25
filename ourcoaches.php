8f83ad64104012127cf07c226f867ac16a01c56c
RamonChambers <RamonChambers@users.noreply.github.com>
2015-11-25 21:49:24 +0000

    Create ourcoaches.php

---
+++ ourcoaches.php
@@ -0,0 +1,144 @@
+
+<?php 
+
+include 'includes/head.php';
+include 'includes/menu.php';
+
+?>	
+
+<div class="maincontainer col-lg-10 col-md-10 col-sm-10 col-xs-10"><!--  maincontainer starts here -->
+
+	<h1>Our Coaches</h1>
+	<div class="outer_container" > <!-- start of outer_container div -->
+	<div class="subcontainer"> <!-- start of subcontainer div -->
+	<img src="images/coaches/1.png">
+	<div class="inner_subcontainer" style="padding:8px;">  <!-- start of inner_subcontainer div -->
+			<h3>Qualifications</h3> 
+			<ul class="container_list" >
+			<li>UEFA A License Part 1</li>
+			<li>UEFA B License</li>
+			<li>FA Futsal Level 2</li>
+			<li>FA Futsal Level 1 </li>
+			<li>FA Youth Award</li>
+			<li>FA Youth Module 3</li>
+			<li>FA Youth Module 2</li>
+			<li>FA Youth Module 1</li>
+			<li>FA Level 2 in Coaching Football</li>
+			<li>FA Level 1 in Coaching Football</li>
+
+
+			</ul>
+			</div> <!-- end of inner_subcontainer div -->
+			</div>  <!-- end of subcontainer div -->
+		<div class="inner_container"> <!-- start of inner_container div -->
+		<h3>Kenny Rickelsford</h3>
+		<p>	Having coached for over nine years, I have learnt many different
+			coaching styles through observing at all levels of the game. I
+			continuously develop as a coach by reading books, game analysis and my
+			dedication to research in sports and psychology. </p>
+
+			<p>Through my experience I have found the player-centred coaching style
+			and a ‘street football’ playing philosophy has a huge influence on my
+			teachings and methodology. </p>
+
+			<p>I try to be the best coach possible as one of the five most common
+			reasons for dropping out of sport for young people are related to the
+			coach; therefore my purpose is to create an environment that is
+			enjoyable.</p>
+
+			<ul class="container_list"  >
+			<strong>Some of my Coaching Philosophies:</strong>
+			<li>Players develop understanding and learn more effectively when they are
+			involved in solving problems for themselves.</li>
+			<li>Players are more productive if they enjoy what they do.</li>
+			<li>Provide individual instruction more so than to everybody as it
+			penetrates better than group instructions.</li>
+			<li>Encourage players to manage their thinking by making decisions promptly.</li>
+			<li>Encourage players to set goals and believe in themselves.</li>
+			<li>Encouraging creativity and imagination.</li>
+
+			</ul>
+	
+		
+			<ul class="container_list">
+				<strong>Current Club:</strong><br>	
+					<li>Dulwich Hamlet F.C</li>
+					<br>
+					<strong>Previous Clubs:</strong><br>
+					<li>Fulham F.C</li>
+					<li>Crystal Palace Ladies F.C </li>
+					<li>Aldershot Town F.C </li>
+					<li>Millwall Lionesses F.C </li>
+					<li>Crawley Town F.C</li>
+					<li>Barnet F.C</li>
+
+			</ul>
+	
+			</div> <!-- end of inner_container div -->
+			
+	</div> <!-- end of outer_container div -->
+	
+		<div class="outer_container"> <!-- start of outer_container div -->
+		<div class="subcontainer">  <!-- start of subcontainer div -->
+		<img src="images/coaches/2.png">
+		<div class="inner_subcontainer" style="padding:8px;">  <!-- start of inner_subcontainer div -->
+			<h3>Qualifications</h3> 
+			<ul class="container_list" >
+			<li>UEFA B License</li>
+			<li>FA Futsal Level 2</li>
+			<li>FA Futsal Level 1 </li>
+			<li>FA Youth Module 2</li>
+			<li>FA Youth Module 1</li>
+			<li>FA Level 1 Goal Keeping Coaching Badge </li>
+			<li>FA Level 2 Coaching Badge </li>
+			<li>FA Level 1 Coaching Badge</li>
+			</ul>
+			</div> <!-- end of inner_subcontainer div -->
+			</div> <!-- end of subcontainer div -->
+
+	<div class="inner_container"> <!-- start of inner_container div -->
+		<h3>Alfie Rowlands-Rickelsford</h3>
+			<p>I decided to begin my coaching journey over 8 years ago and have loved
+				every process. I have increased my learning by attending FA and UEFA
+				accredited courses and establishing links with affiliated coaches and
+				viewing their sessions. My heightened learning comes from reading
+				various sports coaching biographies and sports psychology books.
+			</p>
+
+			<p>I am a great believer of guided discovery which allows players to
+			trial, experiment and learn through their own mistakes with occasional
+			‘controlled’ assistance. Creating a ‘deep learning practice zone’ is
+			vitally important to me. An environment with constant repetitive
+			practices with high tempo allows new techniques, skills and decisions
+			to become subconscious.
+			</p>
+
+			<p><strong>Some of my Coaching Philosophies:</strong></p>
+			<ul class="container_list"  >
+			<li>Making sure we all respect one another. We listen, we share ideas and
+			install the same values and disciplines associated with being in the
+			public eye.</li>
+			<li>Create fun and stimulating practices (avoiding queues/waiting time)etc.</li>
+			<li>Encourage players to make mistakes during practices or games as this
+			will help them develop solutions when faced in similar situations..</li>
+			<li>Praise those who attempt to be creative without fear of failure.</li>
+			<li>Praise those who are persistent in executing new skills and techniques.</li>
+			<li>Encouraging creativity and imagination</li>
+
+			</ul>
+
+			<ul class="container_list">
+			<p><strong>Current Clubs:</strong><br>	
+			<li>AFC Wimbledon</li><br>	
+			<strong>Previous Clubs:</strong>
+			<li>Crystal Palace Ladies F.C</li>
+			<li>Millwall Lionesses F.C</li>
+			</p>
+			</ul>
+			</div> <!-- end of inner_container div -->
+		
+	</div> <!-- end of outer_container div -->
+
+<?php include 'includes/footer.php'; ?>
+
+</div><!--  maincontainer ends here -->
