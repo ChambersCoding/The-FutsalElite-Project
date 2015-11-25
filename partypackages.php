ced5d143c1d1d0e05c355d37ff05a5d3dd668e42
RamonChambers <RamonChambers@users.noreply.github.com>
2015-11-25 21:49:48 +0000

    Create partypackages.php

---
+++ partypackages.php
@@ -0,0 +1,114 @@
+
+<?php 
+
+include 'includes/head.php';
+include 'includes/menu.php';
+
+?>	
+
+<div class="maincontainer col-lg-10 col-md-10 col-sm-10 col-xs-10" ><!--  maincontainer starts here -->
+
+<h1>Our Party Packages </h1>
+	<p>Our Parties are magnificent, marvellous and memorable. Parties can be arranged for your chosen length of time at a venue convenient to the family and guests.
+	 We provide certficates, medals and a trophy for the child who the special day belongs to.<p>
+
+	<!-- RONALDO PACKAGES DETAILS -->
+
+	<div class="col-lg-4 col-md-4 col-sm-0 col-xs-0" style=" height:265px; float:left; margin-top:50px"> <!-- start of image_container div -->
+	<img src="images/ronaldo_package_bw_withoutsmallpictures.png" style="width:100%; height:265px;">
+	</div> <!-- end of image_container div -->
+
+		<div class=" col-lg-8 col-md-8 col-sm-12 col-xs-12" style="height:265px; background: rgb(255, 229, 0); border-radius: 30px 30px 30px 30px; margin-top:50px; ">  <!-- start of inner_subcontainer div -->
+		<h3>The ‘Ronaldo’ Package</h3>
+		<ul class="container_list" >
+		<li>60 minutes of fun activities and competitions</li>
+		<li>30 minutes of matches and tournaments</li>
+		</ul>
+	<div style="width:46%; float:left; ">	<!-- start of div for 1x coach prices  -->
+	<img src="images/icons/coach.png" style="float:left; width:40px; height:46px;"/><h3 style="font-size: 35px; margin: 0px;">£49.99</h3><br> 
+	<p style="margin: 0px; font-size: 13px;">1 Coach (Below 10 participants)</p>
+	<br><div style="background:black; padding: 1px; text-align:center; width: 46%;"><a href="contactus.php">
+	<h3 style="color:white; font-size:25px;">Select</h3></a></div>
+	</div><!-- end of div for 1x coach prices  -->
+	<div style="width:45%; float:left; ">	<!-- start of div for 2x coach prices  -->
+	<img src="images/icons/coach.png" style="float:left; width:40px; height:46px; padding-right:0px;"/><strong style="color:red; float: left; font-size:41px;"> + </strong> 
+	<img src="images/icons/coach.png" style="float:left; width:40px; height:46px;"/><h3 style="font-size: 35px; margin: 0px;">£79.99</h3> <br>
+	<p style="margin: 0px; font-size: 13px;">2 coaches (Above 10 participants)</p>
+	<br><div style="background:black; padding: 1px; text-align:center; width: 46%;"><a href="contactus.php">
+	<h3 style="color:white; font-size:25px;">Select</h3></a></div>
+	</div><!-- end of div for 1x coach prices  -->
+
+		</div> <!-- end of inner_subcontainer div -->
+	<br><br>
+
+	<!-- NEYMAR PACKAGES DETAILS -->
+
+	<div class="col-lg-4 col-md-4 col-sm-0 col-xs-0" style=" height:265px; float:left; margin-top:50px; "> <!-- start of image_container div -->
+	<img src="images/neymar_package_bw_withoutsmallpictures.png" style="width:100%; height:265px;">
+	</div> <!-- end of image_container div -->
+
+	<div class=" col-lg-8 col-md-8 col-sm-12 col-xs-12" style="height:265px; background: rgb(255, 229, 0); border-radius: 30px 30px 30px 30px; margin-top:50px; ">  <!-- start of inner_subcontainer div -->
+	<img src="images/icons/bestsellericon.png" style="width: 70px; height: 70px; float: right;  margin-top:10px;">
+
+		<h3>The ‘Neymar’ Package</h3>
+		<ul class="container_list"  >
+		<li>60 minutes of fun activities and competitions</li>
+		<li>30 minutes of party food (self-catered)</li>
+		<li>30 minutes of matches and tournaments</li>
+		</ul>
+	<div style="width:46%; float:left; ">	<!-- start of div for 1x coach prices  -->
+	<img src="images/icons/coach.png" style="float:left; width:40px; height:46px;"/><h3 style="font-size: 35px; margin: 0px;">£79.99</h3><br> 
+	<p style="margin: 0px; font-size: 13px;">1 Coach (Below 10 participants)</p>
+	<br><div style="background:black; padding: 1px; text-align:center; width: 46%;"><a href="contactus.php">
+	<h3 style="color:white; font-size:25px;">Select</h3></a></div>
+	</div><!-- end of div for 1x coach prices  -->
+	<div style="width:45%; float:left; ">	<!-- start of div for 2x coach prices  -->
+	<img src="images/icons/coach.png" style="float:left; width:40px; height:46px; padding-right:0px;"/><strong style="color:red; float: left; font-size:41px;"> + </strong> 
+	<img src="images/icons/coach.png" style="float:left; width:40px; height:46px;"/><h3 style="font-size: 33px; margin: 0px;">£119.99</h3> <br>
+	<p style="margin: 0px; font-size: 13px; ">2 coaches (Above 10 participants)</p>
+	<br><div style="background:black; padding: 1px; text-align:center; width: 46%;"><a href="contactus.php">
+	<h3 style="color:white; font-size:25px;">Select</h3></a></div>
+	</div><!-- end of div for 1x coach prices  -->
+		</div> <!-- end of inner_subcontainer div -->
+
+	<br><br>
+
+	<!-- MESSI PACKAGES DETAILS -->
+
+	<div class="col-lg-4 col-md-4 col-sm-0 col-xs-0" style=" height:285px; float:left; margin-top:50px; "> <!-- start of image_container div -->
+	<img src="images/messi_package_bw_withoutsmallpictures.png" style="width:100%; height:285px;" >
+	</div> <!-- end of image_container div -->
+
+	<div class=" col-lg-8 col-md-8 col-sm-12 col-xs-12" style="height:285px; background: rgb(255, 229, 0); border-radius: 30px 30px 30px 30px; margin-top:50px;">  <!-- start of inner_subcontainer div -->
+	<img src="images/icons/mostpopularicon.png" style="width: 70px; height: 70px; float: right; margin-top:10px;">
+
+		<h3>The ‘Messi’ Package</h3>
+		<ul class="container_list"  >
+		<li>60 minutes of fun activities and competitions</li>
+		<li>30 minutes of party food (self-catered)</li>
+		<li>30 minutes of matches and tournaments</li>
+		<li>Awards Finale including certificates, medals and trophies</li>
+		</ul>
+	<div  style="width:46%; float:left; ">	<!-- start of div for 1x coach prices  -->
+	<img src="images/icons/coach.png" style="float:left; width:40px; height:46px;"/><h3 style="font-size: 35px; margin: 0px;">£99.99</h3><br> 
+	<p style="margin: 0px; font-size: 13px;">1 Coach (Below 10 participants)</p>
+	<br><div style="background:black; padding: 1px; text-align:center; width: 46%;"><a href="contactus.php">
+	<h3 style="color:white; font-size:25px;">Select</h3></a></div>
+	</div><!-- end of div for 1x coach prices  -->
+	<div style="width:45%; float:left; ">	<!-- start of div for 2x coach prices  -->
+	<img src="images/icons/coach.png" style="float:left; width:40px; height:46px; padding-right:0px;"/><strong style="color:red; float: left; font-size:41px;"> + </strong> 
+	<img src="images/icons/coach.png" style="float:left; width:40px; height:46px;"/><h3 style="font-size: 33px; margin: 0px;">£139.99</h3> <br>
+	<p style="margin: 0px; font-size: 13px;">2 coaches (Above 10 participants)</p>
+	<br><div style="background:black; padding: 1px; text-align:center; width: 46%;"><a href="contactus.php">
+	<h3 style="color:white; font-size:25px;">Select</h3></a></div>
+	</div><!-- end of div for 1x coach prices  -->
+		</div> <!-- end of inner_subcontainer div -->
+	
+
+
+<br><br><br>
+
+<?php include 'includes/footer.php'; ?>
+
+</div><!--  maincontainer ends here -->
+
